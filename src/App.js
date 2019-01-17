7cf61f6a000efdff519cad9c4c4dd25c8aacd8b9
jstntrn <justin.tran2290@gmail.com>
2019-01-17 09:39:25 -0700

    Initial css designs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="App-header">
+          <h2 className="Header-title">Header</h2>
+        </div>
+        <div className="Header-fill"></div>
+        <div className="Card-display">
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 2</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 3</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 4</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 5</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 6</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 7</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 8</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 9</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 10</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+          <div className="Card">
+            <div className="Card-image">Image 1</div>
+          </div>
+         
+        </div>
+        
       </div>
     );
   }
