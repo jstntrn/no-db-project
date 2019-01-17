cdb5de5f40639b3aff1ad8b0755692af61cbf3e6
jstntrn <justin.tran2290@gmail.com>
2019-01-17 16:43:56 -0700

    end of day sace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Components/Card';
+import logo from './taco.png'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <div className="App-header">
+          <img className="logo" src={logo} alt='logo'/>
           <h2 className="Header-title">Tacosss</h2>
           <div className="Search">
             <input className="Searchbar" defaultValue={"Search"} />
