41c432a1e94ac9e6106a30cd72abdb0841d7a2af
jstntrn <justin.tran2290@gmail.com>
2019-01-17 11:23:19 -0700

    card conmponent created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './Components/Card';
 import './App.css';
 
 class App extends Component {
@@ -7,61 +8,44 @@ class App extends Component {
       <div>
         <div className="App-header">
           <h2 className="Header-title">Header</h2>
+          <div className="Search">
+            <input className="Searchbar"/>
+          </div>
         </div>
         <div className="Header-fill"></div>
         <div className="Card-display">
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 2</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 3</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 4</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 5</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 6</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 7</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 8</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 9</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 10</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-          <div className="Card">
-            <div className="Card-image">Image 1</div>
-          </div>
-         
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
         
       </div>
---
+++ src/Components/Card.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+    render(){
+        return(
+            <div className="Card">
+                <img className="Card-image" src={"https://images.unsplash.com/photo-1516822854725-44c5e7833c3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"} alt='taco'/>
+                <p className="Card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id erat at quam vestibulum convallis a facilisis velit.
+                </p>
+                <div className="Card-button-display">
+                    <button className="Card-button">1</button>
+                    <button className="Card-button">2</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
